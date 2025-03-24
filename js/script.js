65b0de3c5f162c0233776dfeb9404eefea0596bc
CamillaKleymen <camillakleymen@gmail.com>
2025-03-24 15:57:57 +0500

    added sections on main page

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    speed: 500,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button type="button" class="slick-prev btn btn-light rounded-circle p-2 position-absolute top-50 start-0 translate-middle-y"><i class="bi bi-chevron-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next btn btn-light rounded-circle p-2 position-absolute top-50 end-0 translate-middle-y"><i class="bi bi-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
