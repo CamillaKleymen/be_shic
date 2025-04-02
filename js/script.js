5a3af171ba5b5726911ea78d1fa9b73458f5d465
CamillaKleymen <camillakleymen@gmail.com>
2025-04-02 14:03:35 +0500

    hostel done

--- js/script.js
+++ js/script.js
@@ -95,10 +95,14 @@ $(document).ready(function(){
       cssEase: "linear",
       slidesToShow: 5,
       slidesToScroll: 1,
-      // prevArrow:
-      //   '<button type="button" class="slick-prev btn btn-light rounded-circle p-2 position-absolute top-50 start-0 translate-middle-y"><i class="bi bi-chevron-left"></i></button>',
-      // nextArrow:
-      //   '<button type="button" class="slick-next btn btn-light rounded-circle p-2 position-absolute top-50 end-0 translate-middle-y"><i class="bi bi-chevron-right"></i></button>',
+      prevArrow:
+        `<button class="btn btn-light rounded-circle me-2" style="width: 48px; height: 48px;">
+            <i class="bi bi-arrow-left"></i>
+        </button>`,
+      nextArrow:
+        `<button class="btn btn-dark rounded-circle" style="width: 48px; height: 48px;">
+            <i class="bi bi-arrow-right"></i>
+        </button>`,
       responsive: [
         {
           breakpoint: 1024,
@@ -125,6 +129,53 @@ $(document).ready(function(){
     });
   });
     
+
+
+ 
+  $(document).ready(function(){
+    $('.cert-slider_mb').slick({
+      dots: false,
+      infinite: true,
+      arrows: true,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      speed: 500,
+      cssEase: "linear",
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow:
+        `<button class="btn btn-light rounded-circle me-2" style="width: 48px; height: 48px;">
+            <i class="bi bi-arrow-left"></i>
+        </button>`,
+      nextArrow:
+        `<button class="btn btn-dark rounded-circle" style="width: 48px; height: 48px;">
+            <i class="bi bi-arrow-right"></i>
+        </button>`,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  }); 
 // function changeImage(element) {
 //   document.getElementById("mainImage").src = element.src;
 //   document.querySelectorAll(".thumb").forEach(img => img.classList.remove("border-primary", "border-3"));
